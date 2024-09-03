0bcbbc1aba240a243f9eff0a1bc5ea85b308404d
Victor <victormwendwa804@gmail.com>
2024-09-03 11:11:02 +0300

    Developing relationship between Use and Address in DB

--- flask_sqlalchemy.py
+++ flask_sqlalchemy.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 # Create an engine that stores data in the local directory's
 engine = create_engine('sqlite:///:memory:', each=True)
@@ -16,6 +17,25 @@ class User(Base):
     fullname = Column(String)
     nickname = Column(String)
 
+# Define a __repr__ method for the User class
     def __repr__(self):
         return "<User(name='%s', fullname='%s', nickname='%s')>" % (
             self.name, self.fullname, self.nickname)
+
+# Define an Address class, mapping relationship to the User class
+class Address(Base):
+    __tablename__ = 'addresses'
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    # Relationship with User
+    user = relationship("User", back_populates="addresses")
+
+    # Define a __repr__ method for the Address class
+    def __repr__(self):
+        return "<Address(email_address='%s')>" % self.email_address
+
+# Add a relationship from User to Address
+User.addresses = relationship(
+    "Address", order_by=Address.id, back_populates="user")
