bf1dcb3b31b1ccfee5883b5eb0e1bf05e315041f
Victor <victormwendwa804@gmail.com>
2024-09-04 14:16:04 +0300

    improving blog admin

--- Django/myproject/blog/admin.py
+++ Django/myproject/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Register your models here
+@admin.register(Post)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'content')
+
+
